e3edd4eb66208f4d14787ed5102d97798806142e
Siphosenkosi Mbala <221140700@mycput.ac.za>
2025-07-26 12:19:12 +0200

    i did Employee

--- src/main/java/ac/za/cput/controller/EmployeeController.java
+++ src/main/java/ac/za/cput/controller/EmployeeController.java
@@ -16,7 +16,7 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController (EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
